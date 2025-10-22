482246b0e929288831abfb13700e51d24eb765d8
Anhnaoto <50966044+Anhnato@users.noreply.github.com>
2025-10-22 22:15:14 +0700

    Build a REST API

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -24,4 +24,13 @@ class Product extends Model
     public function getFormattedTotalAmount($quantity = 1){
         return number_format($this->price * $quantity) .' '. $this->currency;
     }
+
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'currency',
+        'display_image_url',
+        'category_id',
+    ];
 }
