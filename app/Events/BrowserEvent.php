6f8e02631e822ac73b048eb25bc215c3d609b105
Anhnaoto <50966044+Anhnato@users.noreply.github.com>
2025-10-24 22:24:44 +0700

    Homepage

---
+++ app/Events/BrowserEvent.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Events;
+
+class BrowserEvent {
+    public const DISPLAY_OFFCANVAS = 'display-offcanvas';
+
+    public const CLOSE_OFFCANVAS = 'close-offcanvas';
+
+    public const LANGUAGE_CHANGE = 'language-changed';
+
+    public const THEME_CHANGED = 'theme-changed';
+}
---
+++ app/Http/Controllers/HomepageController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Category;
+
+use Illuminate\Http\Request;
+
+class HomepageController extends Controller
+{
+    public function index(){
+        $categories = Category::query()->with('products')->get();
+
+        return view('homepage',['categories'=>$categories]);
+    }
+}
