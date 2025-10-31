accafd0df9e8fe2e49af688b2e9551bb877cf9b3
Anhnaoto <50966044+Anhnato@users.noreply.github.com>
2025-10-31 14:12:22 +0700

    Build the add to cart

---
+++ app/Models/Order.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Facades\URL;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    public function cart(): BelongsTo{
+        return $this->belongsTo(Cart::class);
+    }
+
+    public function getCheckoutConfirmationPath(): string{
+        return URL::signedRoute('checkout.show', ["order"=> $this->id]);
+    }
+}
